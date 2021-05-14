c789b42afba0a032e887547ce3de1dadcc392af5
dhrush <dhrush97@gmail.com>
2021-05-15 02:19:54 +0530

    Tortise Heir technique slow pointer fast pointer linked list method

---
+++ Arrays/DuplicateNosInArray.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) 
+    {
+        int slow = nums[slow];
+        int fast = nums[fast];
+        
+        do
+        {
+            slow = nums[slow];
+            fast = nums[nums[fast]];            
+        }while(slow!=fast);
+            
+        fast = nums[0];
+        while(fast!=slow)
+        {
+            fast = nums[fast];
+            slow = nums[slow];
+        }
+        
+        return slow;
+        
+    }
+};
